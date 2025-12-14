e0381c1ee0a651e23f6119e197708e3bc77d7e61
xcz0 <1355935992@qq.com>
2025-12-14 16:07:32 +0800

    移除自定义 LayerNorm 类，改为使用现有的 LayerNorm 实现

--- scr/Transformer.py
+++ scr/Transformer.py
@@ -1,12 +1,11 @@
 from dataclasses import dataclass
 
-
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
-from torch.nn import Module, Embedding
+from torch.nn import Module
 
 from .Attention import MultiHeadAttention
+from .Norm import LayerNorm
 
 
 @dataclass
@@ -17,20 +16,6 @@ class DecoderConfig:
     emb_dim: int  # 嵌入维度
     drop_rate: float  # dropout 率
     qkv_bias: bool = False  # 查询-键-值偏置
-
-
-class LayerNorm(Module):
-    def __init__(self, normalized_shape, eps=1e-5):
-        super().__init__()
-        self.eps = eps
-        self.scale = nn.Parameter(torch.ones(normalized_shape))
-        self.shift = nn.Parameter(torch.zeros(normalized_shape))
-
-    def forward(self, x):
-        mean = x.mean(dim=-1, keepdim=True)
-        variance = x.var(dim=-1, keepdim=True, unbiased=False)
-        norm_x = (x - mean) / torch.sqrt(variance + self.eps)
-        return self.scale * norm_x + self.shift
 
 
 class FeedForward(Module):
